dc651f51f741572d7ebbdfcd20e7e3028c66f8b6
Ayesha <fxolbird@gmail.com>
2021-03-12 08:14:41 -0800

    'change'

--- src/dropdown.js
+++ src/dropdown.js
@@ -4,48 +4,6 @@ import React, { useState, useEffect } from 'react'
 import NativeSelect from '@material-ui/core/NativeSelect';
 
 
-// export const CountryPicker = () => {
-
-
-//     const [ccdata, setcdata] = useState([])
-//     //    const [cload, csetload] = useState([false])
-//     useEffect(() => {
-//         async function fetchcdata() {
-//             //          csetload(true);
-//             const cresponse = await fetch('https://disease.sh/v3/covid-19/countries');
-//             const cdata = await cresponse.json();
-//         //    console.log(cdata);
-//             setcdata(cdata);
-//             //  csetload(false);
-//         } fetchcdata();
-//     }, [])
-
-
-//     let cc;
-//     let [index, setIndex] = useState(0);
-
-//     return (
-//         <div>
-//             <br></br>
-//             <NativeSelect id="select" onChange={(e) =>{ cc=e.target.value;
-//             index = ccdata.findIndex(x=>x.country===cc);
-//                 setIndex(index);
-//             }}>
-//                 {ccdata && ccdata.map(({ country }, index) => <option key={index} value={country}>{country}</option>)}
-//             </NativeSelect>
-           
-//         </div>
-//     )
-// }
-
-
-
-
-
-// import React,{useEffect,useState} from 'react'
-// import NativeSelect from '@material-ui/core/NativeSelect';
-
-
 export const DropDown = () => {
     const[data,setData] = useState()
     const[fetching,setFetching] = useState(false)
