2ad734b13688917c23cea11d3e8d34443e7f2aef
Ayesha <fxolbird@gmail.com>
2021-03-12 06:56:55 -0800

    'changed'

---
+++ src/dropdown.js
@@ -0,0 +1,108 @@
+
+  
+import React, { useState, useEffect } from 'react'
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+
+export const CountryPicker = () => {
+
+
+    const [ccdata, setcdata] = useState([])
+    //    const [cload, csetload] = useState([false])
+    useEffect(() => {
+        async function fetchcdata() {
+            //          csetload(true);
+            const cresponse = await fetch('https://disease.sh/v3/covid-19/countries');
+            const cdata = await cresponse.json();
+        //    console.log(cdata);
+            setcdata(cdata);
+            //  csetload(false);
+        } fetchcdata();
+    }, [])
+
+
+    let cc;
+    let [index, setIndex] = useState(0);
+
+    return (
+        <div>
+            <br></br>
+            <NativeSelect id="select" onChange={(e) =>{ cc=e.target.value;
+            index = ccdata.findIndex(x=>x.country===cc);
+                setIndex(index);
+            }}>
+                {ccdata && ccdata.map(({ country }, index) => <option key={index} value={country}>{country}</option>)}
+            </NativeSelect>
+           
+        </div>
+    )
+}
+
+
+
+
+
+// import React,{useEffect,useState} from 'react'
+// import NativeSelect from '@material-ui/core/NativeSelect';
+
+
+// export const DropDown = () => {
+//     const[data,setData] = useState()
+//     const[fetching,setFetching] = useState(false)
+
+//   useEffect(() => {
+//     async function fetchCountryData () {
+//           setFetching(true)
+
+//         const CoResp = await fetch('https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z')
+//         console.log('CountryREsp =', CoResp)
+
+//          const coData=  await CoResp.json() 
+
+
+//        // setData(coData);
+//        var arr = []
+//        for (var i =0; i<220; i++) {
+//          arr.push(
+//            coData[i]
+//          )
+//        }
+//        console.log(arr)
+//        setData(arr)
+//        console.log(typeof data)
+//        console.log(data)
+      
+//         setFetching(false);
+
+    
+    
+    
+//         } fetchCountryData();
+
+
+
+//   },[])
+    
+    
+//     return (
+//         <div>
+//    <NativeSelect >
+      
+//    {data && data.map(({country},index) => {
+//     <option key= {index}>
+//         {country}
+//     </option>
+ 
+
+
+//          })}
+   
+
+
+
+//    </NativeSelect>
+            
+//         </div>
+//     )
+// }
+
